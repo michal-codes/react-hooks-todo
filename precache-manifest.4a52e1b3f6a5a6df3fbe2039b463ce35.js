a1c1d26ad38e4fef3eb68ed0be459315f4c883e1
Your Name <you@example.com>
2019-05-08 18:17:14 +0200

    Updates

---
+++ precache-manifest.4a52e1b3f6a5a6df3fbe2039b463ce35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18cb30ddf578b7711ffac723c0cb9401",
+    "url": "/react-hooks-todo/index.html"
+  },
+  {
+    "revision": "8f1e9605f7c40ddbe159",
+    "url": "/react-hooks-todo/static/css/main.b87e65e5.chunk.css"
+  },
+  {
+    "revision": "5ec0a454f7eba053b791",
+    "url": "/react-hooks-todo/static/js/2.7c059e57.chunk.js"
+  },
+  {
+    "revision": "8f1e9605f7c40ddbe159",
+    "url": "/react-hooks-todo/static/js/main.013b1f5e.chunk.js"
+  },
+  {
+    "revision": "b2dfd0c883d968b29e28",
+    "url": "/react-hooks-todo/static/js/runtime~main.68f30e0c.js"
+  }
+]);
